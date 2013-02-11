945ddcd48c0e8d22fdf0b32de76f9bd7732e1de6
eugenp <hanriseldon@gmail.com>
2013-02-11 23:54:24 +0200

    successful test performing read api request

--- src/test/java/org/stackexchange/api/client/QuestionsApiLiveTest.java
+++ src/test/java/org/stackexchange/api/client/QuestionsApiLiveTest.java
@@ -1,10 +1,13 @@
 package org.stackexchange.api.client;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 import java.io.IOException;
 
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Before;
@@ -24,7 +27,13 @@ public class QuestionsApiLiveTest {
 
     @Test
     public final void whenInitialRequestIsPerformed_thenNoExceptions() throws ClientProtocolException, IOException {
-        client.execute(new HttpGet(ApiConstants.API_2_1 + "/questions?order=desc&sort=activity&site=stackoverflow"), new BasicResponseHandler());
+        client.execute(new HttpGet(ApiConstants.API_2_1 + "/questions?order=desc&sort=activity&site=stackoverflow"));
+    }
+
+    @Test
+    public final void whenRequestIsPerformed_thenSuccess() throws ClientProtocolException, IOException {
+        final CloseableHttpResponse httpResponse = client.execute(new HttpGet(ApiConstants.API_2_1 + "/questions?order=desc&sort=activity&site=stackoverflow"));
+        assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(200));
     }
 
 }
