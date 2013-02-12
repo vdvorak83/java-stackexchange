5e0e2cc9ddce649f1e974c9016d1cf8ce45e0ffa
eugenp <hanriseldon@gmail.com>
2013-02-12 23:22:15 +0200

    jackson introduced, some parsing of questions

--- src/main/java/org/stackexchange/api/client/QuestionsApi.java
+++ src/main/java/org/stackexchange/api/client/QuestionsApi.java
@@ -33,4 +33,5 @@ public class QuestionsApi {
             throw new IllegalStateException(ex);
         }
     }
+
 }
--- src/test/java/org/stackexchange/api/client/QuestionsApiLiveTest.java
+++ src/test/java/org/stackexchange/api/client/QuestionsApiLiveTest.java
@@ -1,16 +1,22 @@
 package org.stackexchange.api.client;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 
+import org.apache.http.HttpHeaders;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 public class QuestionsApiLiveTest {
     private QuestionsApi questionsApi;
@@ -36,9 +42,24 @@ public class QuestionsApiLiveTest {
     }
 
     @Test
+    public final void whenRequestIsPerformed_thenOutputIsJson() throws ClientProtocolException, IOException {
+        final CloseableHttpResponse httpResponse = questionsApi.questionsAsResponse();
+        assertThat(httpResponse.getHeaders(HttpHeaders.CONTENT_TYPE)[0].getValue(), containsString("application/json"));
+    }
+
+    @Test
     public final void whenRequestIsPerformed_thenOutputIsCorrect() throws ClientProtocolException, IOException {
         final String responseBody = questionsApi.questions();
         assertThat(responseBody, notNullValue());
+    }
+
+    @Test
+    public final void whenParsingOutputFromQuestionsApi_thenOutputIsParsable() throws ClientProtocolException, IOException {
+        final String questionsAsJson = questionsApi.questions();
+        final ObjectMapper mapper = new ObjectMapper();
+        final JsonNode rootNode = mapper.readTree(questionsAsJson);
+        final ArrayNode questionsArray = (ArrayNode) rootNode.get("items");
+        System.out.println(questionsArray.get(0));
     }
 
 }
