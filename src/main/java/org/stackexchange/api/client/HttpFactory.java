5675f9a06746c3b1aca2c998810c34d3fe0bee32
eugenp <hanriseldon@gmail.com>
2013-11-26 15:09:43 +0200

    cleanup work

--- src/main/java/org/stackexchange/api/client/HttpFactory.java
+++ src/main/java/org/stackexchange/api/client/HttpFactory.java
@@ -4,13 +4,26 @@ import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.ssl.SSLContextBuilder;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingClientConnectionManager;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.impl.conn.SchemeRegistryFactory;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,7 +48,7 @@ public final class HttpFactory {
             builder = builder.disableRedirectHandling();
         }
         try {
-            configureSSLHandlingNew(builder);
+            configureSSLHandling(builder);
         } catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException ex) {
             logger.error("Failed to initialize SSL handling.", ex);
         }
@@ -48,10 +61,45 @@ public final class HttpFactory {
         return rawHttpClient;
     }
 
-    private static void configureSSLHandlingNew(final HttpClientBuilder builder) throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+    private static void configureSSLHandling(final HttpClientBuilder builder) throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
         final SSLContextBuilder sslcb = new SSLContextBuilder();
         sslcb.loadTrustMaterial(KeyStore.getInstance(KeyStore.getDefaultType()), new TrustSelfSignedStrategy());
         builder.setSslcontext(sslcb.build());
+    }
+
+    @SuppressWarnings("deprecation")
+    public static DefaultHttpClient httpClientOld(final boolean followRedirects) {
+        final PoolingClientConnectionManager cxMgr = new PoolingClientConnectionManager(SchemeRegistryFactory.createDefault());
+        cxMgr.setMaxTotal(100);
+        cxMgr.setDefaultMaxPerRoute(20);
+
+        final HttpParams httpParameters = new BasicHttpParams();
+        HttpConnectionParams.setConnectionTimeout(httpParameters, 60 * 1000); // connect to
+        HttpConnectionParams.setSoTimeout(httpParameters, 60 * 1000); // receive data from
+        httpParameters.setParameter("http.connection-manager.timeout", 60 * 1000); // wait for a connection from the pool
+        httpParameters.setParameter("http.protocol.handle-redirects", followRedirects);
+
+        final DefaultHttpClient rawHttpClient = new DefaultHttpClient(cxMgr, httpParameters);
+
+        try {
+            enableSSLOnHttpClientOld(cxMgr);
+        } catch (KeyManagementException | NoSuchAlgorithmException | UnrecoverableKeyException | KeyStoreException e) {
+            throw new IllegalStateException(e);
+        }
+
+        logger.info("Created new Http Client; count: " + ++count);
+        return rawHttpClient;
+    }
+
+    @SuppressWarnings("deprecation")
+    public static final void enableSSLOnHttpClientOld(final ClientConnectionManager connectionManager) throws NoSuchAlgorithmException, KeyManagementException, UnrecoverableKeyException, KeyStoreException {
+        final SSLSocketFactory sf = new SSLSocketFactory(new TrustStrategy() {
+            @Override
+            public boolean isTrusted(final X509Certificate[] certificate, final String authType) throws CertificateException {
+                return true;
+            }
+        }, SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+        connectionManager.getSchemeRegistry().register(new Scheme("https", 443, sf));
     }
 
 }
